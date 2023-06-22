8c4b191c414b6a10a41ccf00ee161d00c06bfeb1
Arman Moaveni <134084996+arimoa@users.noreply.github.com>
2023-06-22 12:30:04 +0330

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var totalEl = document.getElementById("total");
 var remainingEl = document.getElementById("remaining");
 var inputEl = document.getElementById("input");
 var total = 0;
-var remaining = 50;
+var remaining = 60;
 
 inputEl.addEventListener("input", (e) => {
   total = e.target.value.length;
-  remaining = 50 - total;
+  remaining = 60 - total;
   totalEl.innerHTML = `Total Characters: ${total}`;
   remainingEl.innerHTML = `Remaining: ${remaining}`;
 });
