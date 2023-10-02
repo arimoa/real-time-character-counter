ad9cfa629f19072fe6884d4675c6afc4be9ebc08
Arman Moaveni <134084996+arimoa@users.noreply.github.com>
2023-10-02 14:07:30 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var totalEl = document.getElementById("total");
-var remainingEl = document.getElementById("remaining");
-var inputEl = document.getElementById("input");
-var total = 0;
-var remaining = 60;
+const totalEl = document.getElementById("total");
+const remainingEl = document.getElementById("remaining");
+const inputEl = document.getElementById("input");
+let total = 0;
+let remaining = 60;
 
 inputEl.addEventListener("input", (e) => {
   total = e.target.value.length;
